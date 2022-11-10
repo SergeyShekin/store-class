4ef2873c887e932c90cda7eece36c7a4f65ec343
Sergey <myemail@example.com>
2022-11-10 22:31:39 +0300

    ver4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ItemModel from "./Model/ItemModel";
 
 function App() {
 	
-const item = new ItemModel("Tiger of Sweden", "Leonard coat", "Minimalistic coat in cotton-blend", "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.", 399, "£");
+    const item = new ItemModel("Tiger of Sweden", "Leonard coat", "Minimalistic coat in cotton-blend", "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.", 399, "£");
 	
     return (
         <div className="container">
